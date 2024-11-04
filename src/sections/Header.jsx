26780404fa935241201aa136dbaabbcc2d0e9c90
ovi <ataurovishek@gmail.com>
2024-11-04 16:07:34 +0600

    integrated email system

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,17 +1,20 @@
+import { NavLink } from "react-router-dom";
+import { BrowserRouter as Router, } from "react-router-dom";
 
 
 const Header = () => {
     return (
-        <div className="flex justify-center w-full" id="">
-            <nav className="fixed top-5 z-10 flex gap-1 p-0.5 border border-white/15 bg-white/5 rounded-full backdrop-blur">
-                <a href="" className="nav-item">Home</a>
-                <a href="#Project" className="nav-item">Projects</a>
-                <a href="../Pages/About.jsx" className="nav-item">About</a>
-                <a href="" className="nav-item">Articles</a>
-                <a href="#Footer" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">Contact</a>
-            </nav>
-
-        </div>
+        <Router>
+            <div className="flex justify-center w-full" id="">
+                <nav className="fixed top-5 z-10 flex gap-1 p-0.5 border border-white/15 bg-white/5 rounded-full backdrop-blur">
+                    <NavLink to="/" className={({ isActive }) => (isActive ? "nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900" : "nav-item")}>Home</NavLink>
+                    <NavLink to="/projects" className={({ isActive }) => (isActive ? "nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900" : "nav-item")}>Projects</NavLink>
+                    <NavLink to="/about" className={({ isActive }) => (isActive ? "nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900" : "nav-item")}>About</NavLink>
+                    <NavLink to="/articles" className={({ isActive }) => (isActive ? "nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900" : "nav-item")}>Articles</NavLink>
+                    <NavLink to="/Footer" className={({ isActive }) => (isActive ? "nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900" : "nav-item")}>Contact</NavLink>
+                </nav>
+            </div>
+        </Router>
     );
 };
 
